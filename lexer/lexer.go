9fa09ee160d40008eda1c0bba0464483e32f65f9
Aditya-Byte <adityabyte@gmail.com>
2025-03-15 14:42:41 +0530

    Implemented the lexer and define usual tokens

---
+++ lexer/lexer.go
@@ -0,0 +1,144 @@
+package lexer
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func Lexer(input string) []Token {
+	var tokens []Token
+	var currentToken strings.Builder
+	chars := []rune(input)
+	i := 0
+	length := len(chars)
+
+	for i < length {
+		char := chars[i]
+
+		// Skipping the spaces.
+		if unicode.IsSpace(char) {
+			i++
+			continue
+		}
+
+		// Handling comments
+		if char == '/' && i+1 < length && chars[i+1] == '/' {
+			for i < length && chars[i] != '\n' {
+				i++
+			}
+			continue
+		} else if char == '%' {
+			i++
+			for i < length && chars[i] != '%' {
+				i++
+			}
+			i++ // Skipping the last %
+			continue
+		}
+
+		// Handling strings.
+		if char == '"' {
+			currentToken.Reset()
+			i++
+			for i < length && chars[i] != '"' {
+				currentToken.WriteRune(chars[i])
+				i++
+			}
+			tokens = append(tokens, Token{StringLiteral, currentToken.String()})
+			// Resetting the current token
+			currentToken.Reset()
+			i++ // skipping closing "
+			continue
+		}
+
+		// Handling multicharacters
+		if char == '-' && i+1 < length && chars[i+1] == '>' {
+			tokens = append(tokens, Token{PrintOperator, "->"})
+			i += 2
+			continue
+		}
+
+		// Handling single character tokens
+		if isDelimiter(char) {
+			if currentToken.Len() > 0 {
+				tokens = append(tokens, classifyToken(currentToken.String()))
+				currentToken.Reset()
+			}
+
+			switch char {
+			case '=':
+				tokens = append(tokens, Token{AssignOperator, "="})
+			case '(':
+				tokens = append(tokens, Token{LParen, "("})
+			case ')':
+				tokens = append(tokens, Token{RParen, ")"})
+			case '{':
+				tokens = append(tokens, Token{LBrace, "{"})
+			case '}':
+				tokens = append(tokens, Token{RBrace, "}"})
+			case '>':
+				tokens = append(tokens, Token{GreaterThanOperator, ">"})
+			}
+			i++
+			continue
+		}
+
+		// Handling identifiers/keywords/numbers
+		currentToken.WriteRune(char)
+		i++
+
+		// checking the next character is the part of the same token
+		if i < length && !isDelimiterOrSpace(chars[i]) {
+			continue
+		}
+
+		// classify and reset the current token
+		tokens = append(tokens, classifyToken(currentToken.String()))
+		currentToken.Reset()
+	}
+	return tokens
+}
+
+func isDelimiter(char rune) bool {
+	switch char {
+	case '=', '(', ')', '{', '}', '-', '>':
+		return true
+	default:
+		return false
+	}
+}
+
+func isDelimiterOrSpace(char rune) bool {
+	return unicode.IsSpace(char) || isDelimiter(char)
+}
+
+func classifyToken(input string) Token {
+	switch input {
+	case "var":
+		return Token{VarKeyword, input}
+	case "out":
+		return Token{OutKeyword, input}
+	case "if":
+		fmt.Println("I am here dude in the if case.")
+		fmt.Println(input)
+		return Token{IfKeyword, input}
+	case "else":
+		return Token{ElseKeyword, input}
+	}
+
+	if isNumber(input) {
+		return Token{NumberLiteral, input}
+	}
+
+	return Token{Identifier, input}
+}
+
+func isNumber(s string) bool {
+	for _, c := range s {
+		if !unicode.IsDigit(c) {
+			return false
+		}
+	}
+	return true
+}
---
+++ lexer/tokens.go
@@ -0,0 +1,39 @@
+package lexer
+
+type TokenType string
+
+const (
+	// Identifiers and literals 
+	Identifier TokenType = "IDENTIFIER"
+	NumberLiteral TokenType = "NUMBER"
+	StringLiteral TokenType = "STRING"
+
+	// Keywords :
+	VarKeyword TokenType = "VARIABLE"
+	OutKeyword TokenType = "OUTPUT"
+	IfKeyword TokenType = "IF"
+	ElseKeyword TokenType = "ELSE"
+
+	// Operators
+	AssignOperator TokenType = "ASSIGN"
+	PlusOperator TokenType = "PLUS"
+	MinusOperator TokenType = "MINUS"
+	GreaterThanOperator TokenType = "GREATERTHAN"
+	PrintOperator TokenType = "PRINTOPERATOR" // ->
+
+	// Brackets
+	LBrace TokenType = "LEFTBRACE"
+	RBrace TokenType = "RIGHTBRACE"
+	LParen TokenType = "LEFTPARENTHESIS"
+	RParen TokenType = "RIGHTPARENTHESIS"
+
+	// Special Case
+	IllegalToken TokenType = "ILLEGAL"
+)
+
+// Structure of the Token
+
+type Token struct {
+	Type TokenType
+	Value string
+}

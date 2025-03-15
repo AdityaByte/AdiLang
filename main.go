2598f6b2d846b171161f27aee33a07ba8fb5c9d1
Aditya-Byte <adityapawar4914@gmail.com>
2025-03-16 01:13:55 +0530

    minor changes

--- main.go
+++ main.go
@@ -15,16 +15,17 @@ func printToken(tokens []lexer.Token) {
 	}
 }
 
+
 func main() {
 
-	// if len(os.Args) < 2 {
-	// 	fmt.Println("Usage adilang <filename>.adi")
-	// 	return
-	// }
+	if len(os.Args) < 2 {
+		fmt.Println("Usage adilang <filename>.adi")
+		return
+	}
 
-	// filename := os.Args[1]
+	filename := os.Args[1]
 
-	code, err := os.ReadFile("test.adi")
+	code, err := os.ReadFile(filename)
 
 	if err != nil {
 		fmt.Println("Error occured", err)

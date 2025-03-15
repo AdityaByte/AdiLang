2b306bdbac9bfd675386b920ea509725703ace25
Aditya-Byte <adityabyte@gmail.com>
2025-03-16 01:13:03 +0530

    Implemented function executeStatement, executeForLoop

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -6,6 +6,29 @@ import (
 	"github.com/AdityaByte/AdiLang/parser"
 )
 
+func executeStatement(nodes []*parser.ASTNode, env *Environment) error {
+	for _, node := range nodes {
+		switch node.Type {
+		case parser.NodeVariableDeclaration:
+			if err := executeVariableDeclaration(node, env); err != nil {
+				return err
+			}
+		case parser.NodePrint:
+			if err := executePrintStatement(node, env); err != nil {
+				return err
+			}
+		case parser.NodeForLoop:
+			if err := executeForLoop(node, env); err != nil {
+				return err
+			}
+		default:
+			return fmt.Errorf("Unknown statement : %v", node.Type)
+		}
+	}
+
+	return nil
+}
+
 func executeVariableDeclaration(node *parser.ASTNode, env *Environment) error {
 	if node.Type != parser.NodeVariableDeclaration {
 		return fmt.Errorf("expected variable declaration")
@@ -45,20 +68,28 @@ func evaluateExpression(node *parser.ASTNode, env *Environment) (interface{}, er
 	}
 }
 
-func Interpret(ast []*parser.ASTNode, env *Environment) error {
-	for _,node := range ast {
-		switch node.Type {
-		case parser.NodeVariableDeclaration:
-			if err := executeVariableDeclaration(node, env); err != nil {
-				return err
-			}
-		case parser.NodePrint:
-			if err := executePrintStatement(node, env); err != nil {
-				return err
-			} 
-		default:
-			return fmt.Errorf("unknown node type: %s", node.Type)
+func executeForLoop(node *parser.ASTNode, env *Environment) error {
+
+	loopVar := node.Value.(string)
+	rangeNode := node.Children[0]
+	body := node.Children[1]
+	
+	if rangeNode.Type != parser.NodeRange {
+		return fmt.Errorf("expected range")
+	}
+
+	limit := rangeNode.Value.(int)
+	// fmt.Println("limit value ", limit)
+
+	for i := 0; i < limit; i++ {
+		env.Set(loopVar, i)
+		if err := executeStatement(body.Children, env); err != nil {
+			return err
 		}
 	}
 	return nil
+}
+
+func Interpret(ast []*parser.ASTNode, env *Environment) error {
+	return executeStatement(ast, env)
 }
